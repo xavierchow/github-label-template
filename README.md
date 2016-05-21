877ff2a2703b75a491f66427679018bd7a7a23aa
xavierzhou <xiayezhou@gmail.com>
2016-05-21 23:33:15 +0800

    Draft

---
+++ README.md
@@ -0,0 +1,40 @@
+Github Label Template
+------------------------
+
+## Intro
+
+Will you be satisfied and stuck with the default labels in the github repository?
+The answer may probably be NO, you always need to add some labels, customize the colors, etc.
+Finally you will come to an awesome label template(or you might find someone from others),
+when you create a new repository, you might want to use the template you favor, but still manually?
+
+No... This command line tool tries to help you manuplate your github label template effectively.
+
+## Prerequisite
+Node.js 4.4 or later.
+
+## Install
+`npm install github-label-template`
+
+## Usage
+```
+
+  Usage: ghlbl [options]
+
+  Options:
+
+    -h, --help               output usage information
+    -V, --version            output the version number
+    -o, --owner [owner]      repo owner
+    -r, --repo [repo]        repo name
+    -t, --token [token]      github token
+    -e, --export [filename]  export the lables to json file [filename]
+    -d, --del                delete all existed labels
+    -i, --import [filename]  import the labels from json file [filename]
+
+
+```
+
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const Label = require('./lib/label');
+const debug = require('debug')('gh-label-tmpl:index');
+
+program
+  .version('0.0.1')
+  .option('-o, --owner [owner]', 'repo owner')
+  .option('-r, --repo [repo]', 'repo name')
+  .option('-t, --token [token]', 'github token')
+  .option('-e, --export [filename]', 'export the lables to json file [filename]')
+  .option('-d, --del', 'delete all existed labels')
+  .option('-i, --import [filename]', 'import the labels from json file [filename]')
+  //.option('-c, --config [config file]', 'use config file', 'config.json') //TODO or get git info?
+  .parse(process.argv);
+
+if (!program.owner) {
+  program.help((txt) => {
+    console.error('should specify repo onwer!');
+    return txt;
+  });
+}
+if (!program.repo) {
+  program.help((txt) => {
+    console.error('should specify repo name!');
+    return txt;
+  });
+}
+const main = () => {
+  const lbl = new Label(program.owner, program.repo, program.token);
+
+  if (program.export) {
+    const exportedFile = path.resolve(program.export); //FIXME relative path?
+    return lbl.getAll()
+      .then(labels => {
+        fs.writeFileSync(exportedFile, JSON.stringify(labels, null, 2));
+        console.log(`All labels exported to ${exportedFile}`);
+      });
+  }
+  if (program.del) {
+    return lbl.removeAll()
+      .then(() => {
+        console.log('All labels removed.');
+      });
+  }
+  if (program.import) {
+    debug('abs file path: %s', path.resolve(program.import));
+    const labels = require(path.resolve(program.import));
+    return lbl.createAll(labels)
+      .then(() => {
+        console.log('All labels created.');
+      });
+  }
+};
+main();
+
+
